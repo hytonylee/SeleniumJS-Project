f6874c0c29d4c19b6c30b77d37bbba644bc1c847
Tony Lee <hy.tony.lee@gmail.com>
2020-03-13 13:48:06 -0700

    Successfully run the first example

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-'use strict';
-const webdriver = require('selenium-webdriver');
-SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
+const { Builder, By, Key, util } = require('selenium-webdriver');
+require('geckodriver');
 
-const caps = {
-	name: 'Basic Test Example',
-	build: '1.0',
-	version: '70',
-	platform: 'Windows 10',
-	screen_resolution: '1366x768',
-	record_video: 'true',
-	record_network: 'false',
-	browserName: 'Chrome',
-	username: username,
-	password: authkey
+const example = async () => {
+	let driver = await new Builder().forBrowser('firefox').build();
+	await driver.get('http://google.com');
+	await driver.findElement(By.name('q')).sendKeys('Selenium', Key.RETURN);
 };
 
-async function basicExample() {
-	try {
-		var driver = new webdriver.Builder()
-			.usingServer(cbtHub)
-			.withCapabilities(caps)
-			.build();
-
-		await driver.get(
-			'http://crossbrowsertesting.github.io/selenium_example_page.html'
-		);
-
-		await driver.getTitle().then(function(title) {
-			console.log('The title is: ' + title);
-		});
-
-		driver.quit();
-	} catch (err) {
-		handleFailure(err, driver);
-	}
-}
-
-basicExample();
-
-function handleFailure(err, driver) {
-	console.error('Something went wrong!\n', err.stack, '\n');
-	driver.quit();
-}
+example();
