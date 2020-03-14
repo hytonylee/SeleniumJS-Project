05e12d48d615626a3ad906c056669505c9abcaa6
Tony Lee <hy.tony.lee@gmail.com>
2020-03-13 23:36:18 -0700

    Add xpath to dynamc search

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const webdriver = require('selenium-webdriver'),
-	By = webdriver.By,
-	until = webdriver.until;
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
 require('geckodriver');
 const fs = require('fs');
@@ -8,25 +6,26 @@ const faker = require('faker');
 
 const example = async () => {
 	let name = faker.name.findName();
-
-	let driver = await new webdriver.Builder().forBrowser('firefox').build();
-	await driver
-		.manage()
-		.window()
-		.maximize();
-	await driver.manage().deleteAllCookies();
-
-	await driver.get('http://google.com');
-	await driver.findElement(By.name('q')).sendKeys(name, Key.RETURN);
+	let driver = await new Builder().forBrowser('firefox').build();
 
 	try {
-		await driver.takeScreenshot();
-		fs.writeFileSync('img.png', data, 'base64');
+		await driver
+			.manage()
+			.window()
+			.maximize();
+		await driver.manage().deleteAllCookies();
+
+		await driver.get('http://google.com');
+		await driver.findElement(By.name('q')).sendKeys(name, Key.RETURN);
+
+		let pageTwo = await driver.findElement(By.xpath("//*[@id='rso']//h3/a"));
+		pageTwo.click();
 	} catch (err) {
 		console.error(err);
+	} finally {
+		// driver.quit();
+		console.log('complete!');
 	}
-
-	driver.quit();
 };
 
 example();
